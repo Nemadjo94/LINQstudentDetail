46d263a84c1fbc9db76aec397a23d7b67c8c1780
Nemanja <nemadjo94@gmail.com>
2019-07-09 12:19:40 +0200

    Add files via upload

---
+++ LINQstudentDetails/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQstudentDetails
+{
+    public class Program
+    {
+        List<Student> students = new List<Student>()
+        {
+             new Student {FirstName="Tom", LastName="S.", ID=1,
+                          Marks= new List<int>() {97, 92, 81, 60}},
+             new Student {FirstName="Jerry", LastName="M.", ID=2,
+                          Marks= new List<int>() {75, 84, 91, 39}},
+             new Student {FirstName="Bob", LastName="P.", ID=3,
+                          Marks= new List<int>() {88, 94, 65, 91}},
+             new Student {FirstName="Mark", LastName="G.", ID=4,
+                          Marks= new List<int>() {97, 89, 85, 82}},
+        };
+        public List<ContactInfo> contactList = new List<ContactInfo>()
+        {
+            new ContactInfo {ID=111, Email="Tom@abc.com", Phone="9328298765"},
+            new ContactInfo {ID=112, Email="Jerry123@aaa.com", Phone="9876543201"},
+            new ContactInfo {ID=113, Email="Bobstar@aaa.com", Phone="9087467653"},
+            new ContactInfo {ID=114, Email="Markantony@qqq.com", Phone="9870098761"}
+        };
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+
+            IEnumerable<Student> studentQuery =
+                from student in pg.students
+                where student.ID > 0
+                select student;
+
+            Console.WriteLine("Query : Select range_variable");
+            Console.WriteLine("Name : ID");
+
+            foreach(Student s in studentQuery)
+            {
+                s.PrintInfo();
+            }
+        }
+    }
+}
---
+++ LINQstudentDetails/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQstudentDetails
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public List<int> Marks;
+
+        public ContactInfo GetContactInfo(Program pg, int id)
+        {
+            ContactInfo allInfo = 
+                (from ci in pg.contactList
+                 where ci.ID == id
+                select ci).FirstOrDefault();
+
+            return allInfo;
+        }
+
+        private void ReturnMarks()
+        {
+            foreach(int s in Marks)
+            {
+                Console.Write(s.ToString() + " ");
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"First name: {FirstName} {LastName}");
+            Console.WriteLine($"ID: {ID}");
+            Console.Write("Marks: ");
+            ReturnMarks();
+            Console.WriteLine("\n");
+        }    
+    }
+
+}
